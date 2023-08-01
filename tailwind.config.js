511572584ebd8d4af42860143b947741415d127b
hamzi314 <hamzaraja3336@gmail.com>
2023-08-01 12:56:32 +0500

    Final !

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    fontFamily:{
+      Dancing:["Dancing Script","cursive"],
+      
+    },
+   
+    extend: {
+      visibility: ["group-hover"],
+    },
+  },
+  plugins: [],
+}
+
